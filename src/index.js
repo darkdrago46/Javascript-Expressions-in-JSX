f1740ba07b2a00b2e2591a7193e78018a15bba92
darkdrago46 <68347757+darkdrago46@users.noreply.github.com>
2021-12-22 23:46:07 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-var name = "Soumik";
+var fname = "Soumik";
+var lname = "Rakshit";
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {`${fname} ${lname}`}!</h1>
     <p>My Lucky number is {3 + 4}</p>
+    <p>My roll no is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
